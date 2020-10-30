0e7226a354e79b94288ae4988e920d4eadecf40c
Thagoo <lohitgowda56@gmail.com>
2020-10-30 18:31:54 +0000

    Donot set readahead values by default

--- patch/vendor/bin/init.qcom.post_boot.sh
+++ patch/vendor/bin/init.qcom.post_boot.sh
@@ -101,13 +101,13 @@ echo 1 > /sys/module/lpm_levels/lpm_workarounds/dynamic_clock_gating
 echo 1 > /proc/sys/kernel/power_aware_timer_migration
 
 # Set Read ahead values
-dmpts=$(ls /sys/block/*/queue/read_ahead_kb | grep -e dm -e mmc)
-echo 256 > /sys/block/mmcblk0/bdi/read_ahead_kb
-echo 256 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
-echo 256 > /sys/block/sda/queue/read_ahead_kb
-for dm in $dmpts; do
-    echo 256 > $dm
-done
+#dmpts=$(ls /sys/block/*/queue/read_ahead_kb | grep -e dm -e mmc)
+#echo 256 > /sys/block/mmcblk0/bdi/read_ahead_kb
+#echo 256 > /sys/block/mmcblk0rpmb/bdi/read_ahead_kb
+#echo 256 > /sys/block/sda/queue/read_ahead_kb
+#for dm in $dmpts; do
+#    echo 256 > $dm
+#done
 
 # Power saving features
 echo 1 > /sys/devices/soc/${ro.boot.bootdevice}/clkscale_enable
@@ -123,3 +123,4 @@ configure_zram_parameters
 
 # Set EAS parameters
 8917_sched_dcvs_eas
+echo 1 > /sys/module/process_reclaim/parameters/enable_process_reclaim
