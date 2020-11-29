50a48da081275b959d7256fef71cebb9a402e5c6
Thagoo <lohitgowda56@gmail.com>
2020-11-29 13:27:33 +0000

    Cleanup

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # begin properties
 properties() { '
 kernel.string=Triton 4.9 kernel by Thago
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
@@ -28,11 +28,6 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-ui_print "Welcome To Triton"
-ui_print "flavour- Storm Rova(Unified)"
-ui_print "*Built from Proton Clang*"
-ui_print "Linux v4.9 "
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
@@ -54,49 +49,6 @@ ui_print "         Storm              "
 ui_print "* Thago @ xda-developers ***"
 
 patch_cmdline androidboot.usbconfigfs androidboot.usbconfigfs=true
-
-mount -o remount,rw /vendor
-
-ui_print " "
-ui_print "Flashing Custom Thermal-engine conf"
-ui_print " "
-
-# Thermal conf
-cp -f $home/patch/vendor/etc/thermal-engine.conf /vendor/etc/thermal-engine-t.conf
-
-ui_print "Flashing T-Weaks"
-ui_print "Which brings you yet more Optimizations"
-
-# T-Weaks Post boot script
-do_t_weaks() {
-	sed -i '$a chk=$(uname --all | grep -Eio "Triton")' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a if [ "$chk" == "Triton" ]; then' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a \	\if [ -f /vendor/etc/thermal-engine-t.conf ]; then' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a \ 		\mv /vendor/etc/thermal-engine.conf /vendor/etc/thermal-engine.conf~' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a \		\mv /vendor/etc/thermal-engine-t.conf  /vendor/etc/thermal-engine.conf' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a \	\ fi' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a \         \source /vendor/bin/t-weaks.sh' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a else' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a \	\if [ -f /vendor/etc/thermal-engine.conf~ ]; then' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a \ 		\mv /vendor/etc/thermal-engine.conf~ /vendor/etc/thermal-engine.conf' /vendor/bin/init.qcom.post_boot.sh;
-	sed -i '$a fi' /vendor/bin/init.qcom.post_boot.sh;
-}
-
-cp -f $home/patch/vendor/bin/init.qcom.post_boot.sh /vendor/bin/t-weaks.sh;
-chmod 0755 /vendor/bin/t-weaks.sh;
-
-cie=$(grep -Eio "Triton" /vendor/bin/init.qcom.post_boot.sh);
-if [ "$cie" == "Triton" ]; then
-        ui_print "T-Weaks already exits"
-else
-        ui_print " Performing T-Weaks"
-        do_t_weaks;
-fi
-
-ui_print " "
-ui_print "Done! Don't forget to follow @tboxxx for  more updates"
-ui_print "*** Enjoy! *****"
-ui_print "*******************"
 
 write_boot;
 ## end install
